1fc58c01134af7607d9b8d78b0752e2e7e2d11c8
brando <belicarrillo30@gmail.com>
2022-07-22 08:29:09 -0500

    reglas de ruteo, viendo un poco de programacion asyncrona y validadores

---
+++ proyecto_angular_.net/back-end/Controllers/GenerosController.cs
@@ -0,0 +1,66 @@
+﻿using back_end.Entidades;
+using back_end.Repositorios;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace back_end.Controllers
+{
+    [Route("api/generos")]
+    public class GenerosController : ControllerBase
+    {
+        private readonly IRepositorio repositorio;
+
+        public GenerosController(IRepositorio repositorio)
+        {
+            this.repositorio = repositorio;
+        }
+
+        [HttpGet] //api/generos
+        [HttpGet("listado")] //api/generos/listado
+        [HttpGet("/listadogeneros")] // /listadogeneros
+        public ActionResult<List<Genero>> Get()
+        {
+            return repositorio.obtenerTodosLosGeneros();
+        }
+
+        [HttpGet("{id:int}")] //api/genero/id
+        public async Task<ActionResult<Genero>> Get(int id, [FromHeader] string nombre)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var genero = await repositorio.ObtenerPorId(id);
+
+            if (genero == null)
+            {
+                return NotFound();
+            }
+
+            return genero;
+        }
+
+        [HttpPost]
+        public ActionResult Post([FromBody] Genero genero)
+        {
+            return NoContent();
+        }
+
+        [HttpPut]
+        public ActionResult Put([FromBody] Genero genero)
+        {
+            return NoContent();
+        }
+
+        [HttpDelete]
+        public ActionResult Delete()
+        {
+            return NoContent();
+        }
+    }
+}
---
+++ proyecto_angular_.net/back-end/Repositorios/IRepositorio.cs
@@ -0,0 +1,14 @@
+﻿using back_end.Entidades;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace back_end.Repositorios
+{
+    public interface IRepositorio
+    {
+        Task<Genero> ObtenerPorId(int id);
+        List<Genero> obtenerTodosLosGeneros();
+    }
+}
